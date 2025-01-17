3fd7a1a51a5bd79faa61351dd309235a61ee18aa
Anthony GONÇALVES <anthony.goncalves@fms-ea.com>
2025-01-17 09:20:04 +0100

    Ajout projet Spring (backend)

---
+++ backend/src/main/java/fr/toutdouxliste/ToutDouxListeBackendApplication.java
@@ -0,0 +1,13 @@
+package fr.toutdouxliste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToutDouxListeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToutDouxListeBackendApplication.class, args);
+	}
+
+}
